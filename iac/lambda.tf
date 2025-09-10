ad9dcfac52e3d9c0f8146604dfd71014d81b0755
jeancdev <jcode2006@gmail.com>
2025-09-10 03:44:00 -0500

    refactor(lambda): remove unused IAM role and policy document for Lambda function

--- iac/lambda.tf
+++ iac/lambda.tf
@@ -1,22 +1,3 @@
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-#bug to solve!!
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "${var.lambda_function_name}-role-${var.environment}"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
 resource "aws_lambda_permission" "allow_bucket" {
   statement_id  = "AllowExecutionFromS3Bucket"
   action        = "lambda:InvokeFunction"
@@ -52,22 +33,4 @@ resource "aws_lambda_function" "notiapp_processor" {
     Name        = var.lambda_function_name
     Environment = var.environment
   }
-
-  depends_on = [aws_iam_role.iam_for_lambda]
 }
-
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = aws_s3_bucket.notiapp_bucket.id
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.notiapp_processor.arn
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "AWSLogs/"
-    filter_suffix       = ".log"
-  }
-  depends_on = [aws_iam_role.iam_for_lambda]
-
-}
-
-
-
