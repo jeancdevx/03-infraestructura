0b87d9c3d56bd154b74bf1772347fd8b6bd19a83
Jeancarlo Morales <72767265+jeancdevx@users.noreply.github.com>
2025-09-10 00:05:14 -0500

    Revert "feat(infrastructure): add complete Terraform setup for NotiApp (#2)"
    This reverts commit dd0b79ffe3a0c6952b97b47a82740a8daa7928fe.

--- commitlint.config.js
+++
@@ -1,7 +0,0 @@
-/**
- * Commitlint configuration for conventional commits
- * @see https://commitlint.js.org/#/reference-configuration
- */
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-}
