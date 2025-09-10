38d8db2a668ca614b1491daa03ce8bf05179118f
jeancdev <jcode2006@gmail.com>
2025-09-10 03:43:48 -0500

    fix(bucket): correct event casing for S3 bucket notification

--- iac/bucket.tf
+++ iac/bucket.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
 
   lambda_function {
     lambda_function_arn = aws_lambda_function.notiapp_processor.arn
-    events              = ["s3:ObjectCreated:POST"]
+    events              = ["s3:ObjectCreated:Post"]
     filter_prefix       = "AWSLogs/"
     filter_suffix       = ".log"
   }
