ec4b3d4ea4b0b624780fd10381bb34ec2561fb4b
jeancdev <jcode2006@gmail.com>
2025-09-10 03:43:54 -0500

    feat(iam): add IAM role and assume role policy for Lambda

---
+++ iac/iam.tf
@@ -0,0 +1,17 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "${var.lambda_function_name}-role-${var.environment}"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
